7f63897fc748a4854a091a718092ba70eea22b16
Yasir284 <lambawalay@gmail.com>
2022-09-30 20:37:13 +0530

    some changes in css and html

---
+++ script.js
@@ -0,0 +1,6 @@
+// To-do App:
+//Build a simple todo app which adds tasks,edit the tasks and also delete the tasks.
+
+function add() {
+  document.querySelector(".addTask").style.border = "none";
+}
